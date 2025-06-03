0badee47814a9f85ce84fe2ea7244a9d4d580823
vitor-de-matos <107481895+vitor-de-matos@users.noreply.github.com>
2025-06-03 16:57:02 -0300

    adição dos modules no app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { config } from './shared/config/config';
+import { SharedModule } from './shared/shared.module';
+import { AnimalModule } from './animal/animal.module';
+import { FeedingScheduleModule } from './feeding-schedule/feeding-schedule.module';
+import { HabitatModule } from './habitat/habitat.module';
+import { KeeperModule } from './keeper/keeper.module';
+import { MeasurementRecordModule } from './measurement-record/measurement-record.module';
+import { MedicalRecordModule } from './medical-record/medical-record.module';
+import { NutritionModule } from './nutrition/nutrition.module';
+import { QuarentineRecordModule } from './quarentine-record/quarentine-record.module';
+import { SpeciesModule } from './species/species.module';
+import { TransferRecordsModule } from './transfer-records/transfer-records.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+      envFilePath: [__dirname + '/../.env'],
+    }),
+    SharedModule,
+    AnimalModule,
+    FeedingScheduleModule,
+    HabitatModule,
+    KeeperModule,
+    MeasurementRecordModule,
+    MedicalRecordModule,
+    NutritionModule,
+    QuarentineRecordModule,
+    SpeciesModule,
+    TransferRecordsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
